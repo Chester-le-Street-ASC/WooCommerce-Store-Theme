22fe597a387e4a7ad40ff0aef4144624df5bf144
Chris Heppell <clheppell1@sheffield.ac.uk>
2018-01-07 16:34:18 +0000

    CSS Updates

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -20,7 +20,7 @@ if ( ! function_exists( 'understrap_scripts' ) ) {
 
 		//wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), true);
 		//wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_style('chester-bootstap', get_stylesheet_directory_uri() . '/css/bootstrap7.css');
+    wp_enqueue_style('chester-bootstap', get_stylesheet_directory_uri() . '/css/bootstrap8.css');
 
 		if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 			wp_enqueue_script( 'comment-reply' );
